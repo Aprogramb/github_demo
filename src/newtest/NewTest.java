94d3fe3383858fbbf36d5febe16fa32ad1da1e5d
My PC <My PC@192.168.121.184>
2016-10-19 10:41:47 -0500

    Added Hello World

--- src/newtest/NewTest.java
+++ src/newtest/NewTest.java
@@ -15,6 +15,7 @@ public class NewTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("Hello World!");
     }
     
 }
